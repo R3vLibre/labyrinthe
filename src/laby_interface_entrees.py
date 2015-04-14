2f890866a4be018d75fc9ac43cb7ef02742931c8
marky7 <florian.marc@gmx.com>
2015-04-14 14:41:26 +0200

    interface_entree et moteur syntaxiquement corrects

--- src/laby_interface_entrees.py
+++ src/laby_interface_entrees.py
@@ -1,1 +1,31 @@
- 
+# -*- coding: utf-8 -*-
+import pygame
+import pygame.locals as locals #raccourcis
+
+#pygame.init()
+#ecran = pygame.display.set_mode((800,600))
+#liste_actions = ["gauche","droite","haut","bas","quitter"]
+
+
+class Interface_Entrees(object):
+  def __init__(self):
+    self.action = []
+
+  def reception_evenements(self):
+    self.action = []
+    for evt in pygame.event.get():
+      print evt
+      if evt.type == locals.QUIT:
+        self.action.append["quitter"]
+        
+      elif evt.type == locals.KEYDOWN:
+        if evt.key == locals.K_ESCAPE:
+          self.action.append["quitter"]
+        if evt.type == locals.K_UP:
+          self.action.append["haut"]
+        if evt.type == locals.K_DOWN:
+          self.action.append["bas"]
+        if evt.type == locals.K_LEFT:
+          self.action.append["gauche"]
+        if evt.type == locals.K_RIGHT:
+          self.action.append["droite"]
--- src/laby_moteur.py
+++ src/laby_moteur.py
@@ -1,33 +1,34 @@
+# -*- coding: utf-8 -*-
 #demande les entrees qui ont été maintenues
+
+import laby_interface_entrees as ie
+import laby_interface_sorties
+import laby_elements_jeu
+import laby_monde
+import labyrinthe
+import time
  
-def moteur():
-  def (self):
+class Moteur(object):
+  def __init__(self):
     self.jeu_actif=True
-    
-  
-  
-  print "Démarrage de notre démo"
-  jeu_actif = True
-  pygame.init()
-  ecran = pygame.display.set_mode(resolution_ecran)
 
-  fond = pygame.Surface(ecran.get_size())
-  fond = fond.convert() #le stockage interne des donnes graphiques de l'image corresponde au stockage des donnes graphiques de la fennetre
-  #fond.fill((55,150,200))
-  image_fond = pygame.image.load("./assets/fond.jpg")
-  fond = image_fond.convert()
-  
-  ecran.blit(fond,(0,0)) # positionner l'image
-  pygame.display.flip()
+  def commencer(self):
+    while (self.jeu_actif):
+      time.sleep(0.100)
+      action = entrees.reception_evenements()
 
-  while (self.jeu_actif):
-    time.sleep(0.100)
+      if action == "quitter":
+        self.jeu_actif = False
+#      if action == "haut":
+        #deplacement du joueur vers le haut
+#      if action == "bas":
+        #deplacement du joueur vers le bas
+#      if action == "gauche":
+        #deplacement du joueur vers la gauche
+#      if action == "droite":
+        #deplacement du joueur vers la droite
 
-    for evt in pygame.event.get():
-      if evt.type == locals.QUIT:
-	self.jeu_actif = False
-      elif evt.type == locals.KEYDOWN and evt.key == locals.K_ESCAPE:
-	self.jeu_actif = False
-    continue # revient au debout de le boucle (for, while, repeat)
-  
-  return
+
+#moteur_jeu = Moteur()
+#entrees = ie.Interface_Entrees()
+#moteur_jeu.commencer()
