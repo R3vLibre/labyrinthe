95962b9b35d0a6cd15496099b4705541951448da
kévin menet <deftroy@live.fr>
2015-04-15 15:13:31 +0200

    évolution de l'interface et création des objets dans le module teste

--- src/laby_interface.py
+++ src/laby_interface.py
@@ -16,38 +16,83 @@ loc_img = "../assets/"
 images = {"background":"ground.png",
           "joueur":"player.png",
           "mur":"wall.png",
-          "ennemi":"ennemi.png" }
+          "ennemi":"enemi.png" }
 
 
 #liste_actions = ["gauche","droite","haut","bas","quitter"]
 
 class Interface_Entrees(object):
     def __init__(self):
-        self.actions = []
+        return
 
     def reception_evenements(self):
+        actions = []
         for evt in pygame.event.get():
-            print evt
             if evt.type == locals.QUIT:
-                self.actions.append("quitter")
+                actions.append("quitter")
             elif evt.type == locals.KEYDOWN:
                 if evt.key == locals.K_ESCAPE:
-                    self.actions.append("quitter")
-                if evt.type == locals.K_UP:
-                    self.actions.append("haut")
-                if evt.type == locals.K_DOWN:
-                    self.actions.append("bas")
-                if evt.type == locals.K_LEFT:
-                    self.actions.append("gauche")
-                if evt.type == locals.K_RIGHT:
-                    self.actions.append("droite")
-        return self.actions
+                    actions.append("quitter")
+                if evt.key == locals.K_UP:
+                    actions.append("haut")
+                if evt.key == locals.K_DOWN:
+                    actions.append("bas")
+                if evt.key == locals.K_LEFT:
+                    actions.append("gauche")
+                if evt.key == locals.K_RIGHT:
+                    actions.append("droite")
+        return actions
 
 class Interface_Sorties(object):
     def __init__(self):
+        self.preparer_background()
 
-        image_fond = pygame.image.load(loc_img + images["background"])
-        image_fond.convert()
+        self.afficher_elements()
+        return
+
+    def preparer_background(self):
+        self.image_fond = pygame.image.load(loc_img + images["background"])
+        self.image_fond.convert()
+
+
+# Qu'a besoin le moteur ?
+# Qu'a besoin l'interface ?
+    # la désigation de l'élément pour savoir quelle image afficher.
+        # ---> element : dictionnaire ?                                elements = {"joueur":(0,0) , "ennemi":(5,7), "mur": [(4,4), (6,9)]}
+        # elements = [ Joueur("Joueur", (4,7)), Ennemi("Ennemi", (5,7)),
+    # les coordonnées de l'élément pour pouvoir l'afficher.
+
+    def receptionner_element(self, elements):
+        print "à quoi est-ce que je ressemble ?"
+
+        for elem in elements:
+           img_elem = pygame.image.load(loc_img + images[elem.key()])
+           images
+
+        if (element["joueur"] == "joueur"):
+            self.player = pygame.image.load(loc_img + images["joueur"])
+            self.player.convert()
+        if (element[0] == "mur"):
+            self.wall = pygame.image.load(loc_img + images["mur"])
+            self.player.convert()
+        if (element[0] == "ennemi"):
+            self.enemi = pygame.image.load(loc_img + images["ennemi"])
+            self.enemi.convert()
+
+
+    def positionner_elements(self):
+        print "cet élement va à cet position"
+
+
+
+    def preparer_affichage(self):
+        print "préparation à l'affichage des éléments"
+
+    def afficher_elements(self):
+        print "afficher et réafficher les éléments"
+        ecran.blit(self.image_fond, (0,0))
+        pygame.display.flip()
+        print "vient là"
 
 # 1ère approche n'allant pas avec les autres modules :
 
@@ -64,7 +109,7 @@ class Interface_Sorties(object):
 # c'est le moteur qui agit :
         # initialisation de l'interface de sortie par le moteur.
         # interaction avec le moteur :
-            # afficher_background()
+            # préparer_background()
             # récupérer_coordonnées_monde()
             # transformer_coordonnées_monde()
             # réceptionner_coordonnées_éléments() <- utilisation de flags ?
@@ -74,5 +119,3 @@ class Interface_Sorties(object):
             # réafficher_éléments()
 
 
-        ecran.blit(image_fond, (0,0))
-        pygame.display.flip()
--- tests/test02_deplacement_image/test02_deplacements_image.py
+++ tests/test02_deplacement_image/test02_deplacements_image.py
@@ -1,2 +1,79 @@
 # -*- coding: utf-8 -*-
+import sys, os
+print sys.path[0]
+sys.path.append(os.path.join(sys.path[0], "../../src/"))
+print sys.path
 import laby_interface as interface
+import time
+
+def main():
+    print "mise en marche du module de test : simulation du moteur"
+
+    print "Appelle de l'interface d'entrée :"
+    teste_if_entree = interface.Interface_Entrees()
+    print "test d'élément de jeu, appelle de l'interface de sortie :"
+    teste_if_sortie = interface.Interface_Sorties()
+    jeu_actif = True
+    print "je suis ici"
+
+    while (jeu_actif):
+        time.sleep(0.100)
+        actions = teste_if_entree.reception_evenements()
+        print actions
+
+        for evt in actions:
+            print "jeu_actif d = " + str(jeu_actif)
+            if evt == "quitter":
+                jeu_actif = False
+            if evt == "haut":
+                print "haut"
+                #deplacement du joueur vers le haut
+            if evt == "bas":
+                print "bas"
+                #deplacement du joueur vers le bas
+            if evt == "gauche":
+                print "gauche"
+                #deplacement du joueur vers la gauche
+            if evt == "droite":
+                print "droite"
+                #deplacement du joueur vers la droite
+            print "jeu_actif f = " + str(jeu_actif)
+    return
+
+class Element_Jeu(object):
+    def __init__(self, nom, position):
+        self.categorie = None
+        self.nom = nom
+        self.position = position
+        return
+
+class Joueur(Element_Jeu):
+    def __init__(self, nom, position):
+        super(Joueur,self).__init__(nom)
+        self.categorie = "Joueur"
+
+class Mur(Element_Jeu):
+    def __init__(self, nom, position):
+        super(Mur,self).__init__(nom)
+        self.categorie = "Mur"
+
+class Ennemi(Element_Jeu):
+    def __init__(self, nom, position):
+        super(Ennemi,self).__init__(nom)
+        self.categorie = "Mur"
+
+joueur = Joueur("The Hero", (4,3))
+ennemi = Ennemi("Predator", (8,9))
+mur1 = Mur("Mur brique", (6,9))
+mur2 = Mur("Palissade bois", (7,9))
+
+liste_elements = [joueur, ennemi, mur1, mur2]
+
+#   def
+#        test_element = interface.Interface_Sorties()
+#        test_sortie_element = test_element.receptionner_element()
+#        test_receptionner = test_element.positionner_element()
+#   return
+
+if __name__ == '__main__':
+    main()
